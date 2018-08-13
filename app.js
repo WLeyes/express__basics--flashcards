c196087a0a2b59a99c3b8521d223023355760c49
Warren Leyes <warren.leyes@gmail.com>
2018-08-12 18:53:51 -0700

    added cookie-parser started to add middleware logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const bodyParser = require('body-parser');
 
+// Cookie parser
+const cookieParser = require('cookie-parser');
+
 // CREATE EXPRESS SERVER
 
 // Call express function
@@ -13,18 +16,57 @@ const app = express();
 // The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Envoke cookieParser
+app.use(cookieParser());
+
 // Set templating engine
 app.set('view engine', 'pug');
 
+
+app.use((req, res, next) => {
+  req.message = 'This message was passed from middleware 1';
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(req.message);
+  next();
+});
+
 // get home route (since using pug, default looks for folder named views)
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
+});
 
 // get hello route
-app.get('/hello', (req, res) => res.render('hello'));
+app.get('/hello', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
 
 // post hello route
 app.post('/hello', (req, res) => {
-  res.render('hello', { name: req.body.username });
+  // Set cookie with username from input
+  res.cookie('username', req.body.username);
+  // Return response to the hello route passing in the name
+  res.redirect('/');
+});
+
+// post logout route
+app.post('/goodbye', (req, res) => {
+  // Clear cookie with username from input
+  res.clearCookie('username');
+  // redirect to hello to log in
+  res.redirect('/hello');
 });
 
 //  get cards route
